ae1ba600094b3483e055db18f9ccd3f020ef2db8
Prashant Chand <prashantchand2001@gmail.com>
2023-04-15 13:12:50 +0530

    crudproject

---
+++ app/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render,redirect
+from .models import Student
+# Create your views here.
+def index(request):
+    data = Student.objects.all()
+    print(data)
+    context={"data":data}
+    return render(request,"index.html",context)
+
+
+def insertData(request):
+    
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        age=request.POST.get('age')
+        gender=request.POST.get('gender')
+    print(name,email,age,gender)
+    query = Student(name=name,email=email ,age=age,gender=gender)
+    query.save()
+    return redirect("/")
+    return render(request,"index.html")
+
+def updateData(request,id):
+    
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        age=request.POST.get('age')
+        gender=request.POST.get('gender')
+        edit = Student.objects.get(id=id)
+        edit.name=name
+        edit.email=email
+        edit.gender=gender
+        edit.age=age
+        edit.save()
+        return redirect("/")
+        
+    print(name,email,age,gender)
+    query = Student(name=name,email=email ,age=age,gender=gender)
+    query.save()
+    return redirect("/")
+    
+    d = Student.objects.get(id=id)
+    context={"d":d}
+    return render(request,"edit.html",context)
+
+def deleteData(request,id ):
+    d = Student.objects.get(id=id)
+    d.delete()
+    
+    return redirect("/")
+
+ 
+def about(request):
+    return render(request,"about.html")
+
+
+
